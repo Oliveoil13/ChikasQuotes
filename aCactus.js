887e9bb7b5e3928367b7bb405528bc502a23bab8
Oliveoil13 <olivequinn13@gmail.com>
2023-12-18 20:35:55 -0700

    make it accept only numbers

--- aCactus.js
+++ aCactus.js
@@ -1,5 +1,5 @@
 function changeMode(){
-    var element = document.body;
+    let element = document.body;
     element.classList.toggle("dark-mode");
     
     if(element.className == "dark-mode"){
@@ -9,4 +9,16 @@ function changeMode(){
     }
     
     document.getElementById("modeText").innerHTML = modeText;
+}
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+      return false;
+    return true;
+  }
+
+function getQuote(){
+    let quote = 2000;
+    document.getElementById("Total").innerHTML = quote;
 }
