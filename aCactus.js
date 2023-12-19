670304e5f61ff914542f7dde67af3eb9244ac187
Oliveoil13 <olivequinn13@gmail.com>
2023-12-19 15:17:13 -0700

    adding functianality

--- aCactus.js
+++ aCactus.js
@@ -1,8 +1,16 @@
 const Values = [
     ['Normal', .10, .215, .32, .46, .54],
-    ['Deep', .20, .46, .68, .75 , .82]
+    ['Deep', .20, .46, .68, .75 , .82],
 
 ];
+const Refridgerator = [
+    [30, 40],
+    [50, 70],
+];
+
+const OvenNumbers = [40, 60];
+
+const CabinetsNumbers = [100, 50];
 
 function changeMode(){
     let element = document.body;
@@ -16,6 +24,8 @@ function changeMode(){
     
     document.getElementById("modeText").innerHTML = modeText;
 }
+
+
 
 function isNumberKey(evt) {
     var charCode = (evt.which) ? evt.which : evt.keyCode
@@ -58,11 +68,11 @@ let frequency = document.querySelector("#frequency");
 let frequencyvalue = 1;
 
 frequency.addEventListener("change", () =>{
-    frequencyvalue = frequency.options[frequency.selectedIndex].value;
+    frequencyvalue = parseFloat(frequency.options[frequency.selectedIndex].value)|| 0;
 });
 //distance in miles
 let distanceAway = document.querySelectorAll('input[name="Distance"]');
-let distanceAwayvalue = 1;
+let distanceAwayvalue = 0;
 
 distanceAway.forEach(radio => {
     radio.addEventListener("change", () => {
@@ -118,7 +128,7 @@ fridges.addEventListener("input", () => {
 });
 //size of fridges
 let fridgeSize = document.querySelectorAll('input[name="sizeFridge"]');
-let fridgeSizevalue = 0;
+let fridgeSizevalue = 1;
 
 fridgeSize.forEach(radio => {
     radio.addEventListener("change", () => {
@@ -128,7 +138,7 @@ fridgeSize.forEach(radio => {
 });
 //how Dirty is fridge
 let fridgeLevel = document.querySelectorAll('input[name="dirtyFridge"]');
-let fridgeLevelvalue = 1;
+let fridgeLevelvalue = 0;
 
 fridgeLevel.forEach(radio => {
     radio.addEventListener("change", () => {
@@ -148,7 +158,7 @@ blindsSize.forEach(radio => {
 });
 //how Dirty are blinds
 let blindsLevel = document.querySelectorAll('input[name="dirtyBlinds"]');
-let blindsLevelvalue = 1;
+let blindsLevelvalue = 0;
 
 blindsLevel.forEach(radio => {
     radio.addEventListener("change", () => {
@@ -213,9 +223,134 @@ emptyCabinets.forEach(radio => {
     });
 });
 
+//resets page
+function resetForm() {
+    // Reset square feet
+    squareFeetEntry.value = 0;
+    
+    // Reset type of cleaning
+    typeOfCleaning.forEach(radio => {
+        radio.checked = radio.value === "0"; // Assuming "0" is the default value
+    });
+    typeOfCleaningvalue = 0;
+    
+    // Reset level of dirty
+    levelOfDirty.forEach(radio => {
+        radio.checked = radio.value === "1"; // Assuming "1" is the default value
+    });
+
+    // Reset frequency
+    frequency.selectedIndex = 0; // Assuming the first option is the default
+
+    // Reset distance in miles
+    distanceAway.forEach(radio => {
+        radio.checked = radio.value === "0"; // Assuming "1" is the default value
+    });
+
+    // Reset kitchens
+    kitchens.value = 1;
+
+    // Reset do they have children
+    isChildren.forEach(radio => {
+        radio.checked = radio.value === "0"; // Assuming "0" is the default value
+    });
+    isChildrenvalue = 0;
+
+    // Reset do they have pets
+    isPets.forEach(radio => {
+        radio.checked = radio.value === "0"; // Assuming "0" is the default value
+    });
+    isPetsvalue = 0;
+
+    // Reset blinds
+    blinds.value = 0;
+
+    // Reset windows
+    windows.value = 0;
+
+    // Reset fridges
+    fridges.value = 0;
+
+    // Reset size of fridges
+    fridgeSize.forEach(radio => {
+        radio.checked = radio.value === "1"; // Assuming none is the default value
+    });
+    fridgeSizevalue = 1;
+
+    // Reset how dirty is fridge
+    fridgeLevel.forEach(radio => {
+        radio.checked = radio.value === "0"; // Assuming "1" is the default value
+    });
+    fridgeLevelvalue = 0;
+
+    // Reset size of blinds
+    blindsSize.forEach(radio => {
+        radio.checked = radio.value === "5";
+    });
+    blindsSizevalue = 5;
+
+    // Reset how dirty are blinds
+    blindsLevel.forEach(radio => {
+        radio.checked = radio.value === "0";
+    });
+    blindsLevelvalue = 0;
+
+    // Reset ovens
+    ovens.value = 0;
+
+    // Reset how dirty is oven
+    ovenLevel.forEach(radio => {
+        radio.checked = radio.value === "0"; // Assuming "1" is the default value
+    });
+    ovenLevelvalue = 0;
+
+    // Reset clean washer and dryer
+    washerDryer.forEach(radio => {
+        radio.checked = radio.value === "0"; // Assuming "0" is the default value
+    });
+    washerDryervalue = 0;
+
+    // Reset clean dish washer
+    cleanDisher.forEach(radio => {
+        radio.checked = radio.value === "0"; // Assuming "0" is the default value
+    });
+    cleanCabinetsvalue = 0;
+
+    // Reset clean inside cabinets
+    cleanCabinets.forEach(radio => {
+        radio.checked = radio.value === "0"; // Assuming "0" is the default value
+    });
+    cleanCabinetsvalue = 0;
+
+    // Reset are the cabinets empty
+    emptyCabinets.forEach(radio => {
+        radio.checked = radio.value === "1"; // Assuming "1" is the default value
+    });
+    emptyCabinetsvalue = 1;
+
+    // Reset the total quote display
+    document.getElementById("Total").innerHTML = "Enter Square Feet";
+}
+
+
 //math for the quote
 function getQuote(){
+    let quote = (
+        (squareFeet * Values[typeOfCleaningvalue][levelOfDirtyvalue]) * frequencyvalue
+      ) +
+      distanceAwayvalue + (kitchensvalue * 50) + (windowsvalue * 5) +
+      (blindsvalue * blindsSizevalue) +//add blindLevel here after asking 
+      (fridgesvalue * Refridgerator[fridgeSizevalue][fridgeLevelvalue]) +
+      (ovensvalue * ovenLevelvalue) +
+      (washerDryervalue * 50) +
+      (cleanDishervalue * 30) +
+      (cleanCabinetsvalue * CabinetsNumbers[emptyCabinetsvalue])
+      ;
     
-    let quote = "$" + (squareFeet * Values[typeOfCleaningvalue][levelOfDirtyvalue]);
+    if (quote < 100){
+        quote = "$" + 100;
+    }else{
+        quote = "$" + quote;
+    }
     document.getElementById("Total").innerHTML = quote;
 }
